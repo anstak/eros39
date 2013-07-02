9b121e9feba830ca99392a67a9004aad8a1f00d5
anstak <anstak@gmail.com>
2013-07-02 08:38:04 +0300

    new spree project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eros39::Application.config.secret_token = '372dccbc799a10ca649e393fce45c44423457fa86c391ddc06bcb7110a183864c1c80f6920873d6df5ded38862a1c68336df3f5a899d3a88d9cb37e9d0a00755'
